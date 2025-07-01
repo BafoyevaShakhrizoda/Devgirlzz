6600cbb36aecb219b26bd6e240a41c8ad68be4a9
Shahrizoda <saidovashaxnoza092@gmail.com>
2025-07-01 23:53:55 +0500

    sdff

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.common.models import Test, Question,Option,AssessmentResult,Vacancy,Company
+
+admin.site.register(Test)
+admin.site.register(Question)
+admin.site.register(Option)
+admin.site.register(AssessmentResult)
+admin.site.register(Vacancy)
+admin.site.register(Company)
+
